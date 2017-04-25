1bbb5250d3447bde4e748a060231e357e03591cd
Aigerimmsadir <aigerimsadir@gmail.com>
2017-04-26 02:28:13 +0600

    Add files via upload

---
+++ randomball/randomball/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace randomball
+{
+    public partial class Form1 : Form
+    {
+     
+       Graphics g;
+        ball b = new ball();
+    Pen pen = new Pen(Color.Red);
+     
+        public Form1()
+        {
+            this.Size = new Size(600,600);
+            InitializeComponent();
+    
+          
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            while (true)
+            {
+                e.Graphics.Clear(Color.White);
+                e.Graphics.FillPath(pen.Brush, b.gp);
+                b.Move();
+            }
+        }
+    }
+}
---
+++ randomball/randomball/ball.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace randomball
+{
+    class ball
+    {
+        public Point location;
+        public List<Point> neighbours= new List<Point>();
+        public GraphicsPath gp = new GraphicsPath();
+        public ball()
+        {
+            location = new Point(new Random().Next(0,200), new Random().Next(0,200));
+            Randomlocation();
+        }
+
+        public void Randomlocation()
+        {
+            if (location.X >= 1 && location.Y >= 1)
+            {
+                neighbours.Add(new Point(location.X + 5, location.Y));
+                neighbours.Add(new Point(location.X - 5, location.Y));
+                neighbours.Add(new Point(location.X, location.Y + 5));
+                neighbours.Add(new Point(location.X, location.Y - 5));
+                neighbours.Add(new Point(location.X + 5, location.Y - 5));
+                neighbours.Add(new Point(location.X + 5, location.Y - 5));
+                neighbours.Add(new Point(location.X + 5, location.Y + 5));
+                neighbours.Add(new Point(location.X - 5, location.Y - 5));
+            }
+            else location = new Point(new Random().Next(0,200) , new Random().Next(0,200) );
+        }
+
+        public void Move()
+        {
+            gp.Reset();
+          
+            location = neighbours[new Random().Next(0, 7)];
+            gp.AddEllipse(location.X, location.Y, 10, 10);
+            Thread.Sleep(new Random().Next(0, 500));
+            neighbours.Clear();
+            Randomlocation();
+         
+        }
+    }
+}
